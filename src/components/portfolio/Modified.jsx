ade9df5d8fca1d2e42cf90ce80c79e06fa2a85a7
Jenish Limbu <jlimbu539@gmail.com>
2024-12-09 14:49:47 +0545

    updated spacing on contributed projects

--- src/components/portfolio/Modified.jsx
+++ src/components/portfolio/Modified.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const Modified = ({ updatedProjectsLists }) => {
     return (
-        <div className="grid md:grid-cols-[50%_50%] gap-x-2 lg:grid-cols-[33.33%_33.33%_33.33%] mt-4">
+        <div className="grid md:grid-cols-[50%_50%] gap-x-2 gap-y-2 lg:grid-cols-[33.33%_33.33%_33.33%] mt-4">
             {updatedProjectsLists.map((project, key) => (
                 <div key={key} className='border border-slate-200 p-4 '>
                     <a href={project.link} target='_blank'>
                         <img src={project.img} alt="" style={{ height: "200px", width: "100%", objectFit: "cover" }} />
-
                     </a>
                 </div>
             ))}
